75622822f7d2a820a838d5a708422b6e580662ac
gull2112 <163505052+gull2112@users.noreply.github.com>
2024-04-05 15:30:08 +0900

    수정사항
    수정사항

--- 5주차 퀴즈.py
+++ 5주차 퀴즈.py
@@ -1,7 +1,13 @@
-age = int(input("나이를 입력하세요:"))
-if age > 19 :
-    print("성인입니다")
+a= int(input("점수"))
+if a>= 71 :
+    print("A")
+elif a>= 41 :
+    print("B")
+elif  a>=11 :
+    print("C")
+elif a<=10 :
+    print("D")
 
-else :
-    print("미성년자입니다")
-    
+
+
+
